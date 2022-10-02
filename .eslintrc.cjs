1602ff1ab9b0c00cdb17f6bff70ff51bde3cb352
mrcosgun <muhammetrasit.cosgun@reateknoloji.com>
2022-10-02 22:15:09 +0300

    edit project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,19 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
+  env: {
+    browser: true,
+    amd: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+};
